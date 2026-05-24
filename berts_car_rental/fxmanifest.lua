e0a61c276658220869de1acba26db2dc32d01a55
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2026-05-24 18:08:02 -0400

    Add files via upload

---
+++ berts_car_rental/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Popcorn Roleplay/FalseHopeDesigns/imperial-studio - Inspired by VerpiMods map'
+description 'Berts Vehicle Rentals'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore "**"
---
+++ bertsmule/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Bertsmule Addon Car'
+author 'static666/Taco - Popcorn RP'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/**',
+    'data/**',
+}
+
+data_file 'VEHICLE_METADATA_FILE'   'data/vehicles.meta'
+data_file 'HANDLING_FILE'           'data/handling.meta'
+data_file 'VEHICLE_VARIATION_FILE'  'data/carvariations.meta'
+data_file 'CARCOLS_FILE'            'data/carcols.meta'
+
+escrow_ignore "**"
